1e82e1773efcf5e2f044346308d5c7fdda673e2b
Aleksandr <aleksandrphilimonov@gmail.com>
2022-01-30 15:55:01 +0300

    6. Кодировка. [#862]

--- src/main/java/ru/job4j/io/chat/ConsoleChat.java
+++ src/main/java/ru/job4j/io/chat/ConsoleChat.java
@@ -1,6 +1,16 @@
 package ru.job4j.io.chat;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ConsoleChat {
 
@@ -15,20 +25,67 @@ public class ConsoleChat {
         this.botAnswers = botAnswers;
     }
 
-    public void run() {
+    public void run() throws IOException {
+        List<String> answerList = readPhrases();
+        List<String> chat = new ArrayList<>();
+        boolean flag = true;
 
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter message for you PCbot: ");
+            String line = reader.readLine();
+            while (!line.equals(OUT)) {
+                String phrase = "YOU: " + line + "\n";
+                chat.add(phrase);
+                while (!line.equals(STOP) && flag) {
+                    Random random = new Random();
+                    int randomPhraseIndex = random.nextInt(answerList.size());
+                    String pcAnswer = "PC: " + answerList.get(randomPhraseIndex);
+                    System.out.println(pcAnswer);
+                    chat.add(pcAnswer);
+                    chat.add(System.lineSeparator());
+                    line = reader.readLine();
+                    if (line.equals(STOP)) {
+                        flag = false;
+                    }
+                    if (line.equals(OUT)) {
+                        saveLog(chat);
+                        return;
+                    }
+                }
+                line = reader.readLine();
+                if (line.equals(CONTINUE)) {
+                    flag = true;
+                }
+            }
+            saveLog(chat);
+        }
     }
 
-    private List<String> readPhrases() {
-        return null;
+    private List<String> readPhrases() throws IOException {
+        List<String> rsl = new ArrayList<>();
+
+        Path pathAnswerPhrases = Paths.get(botAnswers);
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathAnswerPhrases.toFile()))) {
+            reader.lines().forEach(rsl::add);
+        }
+        return rsl;
     }
 
-    private void saveLog(List<String> log) {
-
+    private void saveLog(List<String> log) throws IOException {
+        Path logPath = Paths.get(path);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(logPath.toFile()))) {
+            for (String str : log) {
+                writer.write(str);
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        ConsoleChat cc = new ConsoleChat("", "");
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("chat.txt");
+        String pathName = path.toString();
+        Path botAnswers = Paths.get("botAnswersName.txt");
+        String botAnswersName = botAnswers.toString();
+        ConsoleChat cc = new ConsoleChat(pathName, botAnswersName);
         cc.run();
     }
 }
