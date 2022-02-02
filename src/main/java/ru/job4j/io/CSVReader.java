f002e6fa28287b7d239abb04d4d8ec8010fee86e
Aleksandr <aleksandrphilimonov@gmail.com>
2022-02-02 15:07:42 +0300

    7. Scanner [#504791 #258808]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -7,21 +7,7 @@ import java.util.Scanner;
 
 public class CSVReader {
     public static void main(String[] args) throws Exception {
-        ArgsName argsName = ArgsName.of(new String[]{
-                "-path=" + new File(args[0]).getAbsolutePath(), "-delimiter=" + args[1], "-out=" + new File(args[2]).getAbsolutePath(), "-filter" + args[3]});
-
-        String data = String.join(
-                System.lineSeparator(),
-                "name;age;last_name;education",
-                "Tom;20;Smith;Bachelor",
-                "Jack;25;Johnson;Undergraduate",
-                "William;30;Brown;Secondary special"
-        );
-        File file = new File(args[0]);
-        try (PrintWriter writer = new PrintWriter(new FileWriter(file))) {
-            writer.println(data);
-        }
-
+        ArgsName argsName = ArgsName.of(args);
         CSVReader.handle(argsName);
     }
 
@@ -38,7 +24,7 @@ public class CSVReader {
         String line = scanner.nextLine();
         String[] lineArray = line.split(delim);
         for (int i = 0, j = 0; j < indexes.length; i++) {
-            if (lineArray[i].equals(filter[j])) {
+            if (filter[j].equals(lineArray[i])) {
                 indexes[j] = i;
                 j++;
             }
@@ -47,7 +33,7 @@ public class CSVReader {
         scanner.close();
 
         Scanner scanner1 = new Scanner(new File(src));
-        if (dest.equals("stdout")) {
+        if ("stdout".equals(dest)) {
             while (scanner1.hasNextLine()) {
                 String[] lines = scanner1.nextLine().split(", ");
                 for (int i = 0; i < indexes.length; i++) {
