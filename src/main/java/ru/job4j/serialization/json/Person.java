e57af8c8e3ce0ff74793f3423dff21ec91f44e65
Aleksandr <aleksandrphilimonov@gmail.com>
2022-02-03 12:45:52 +0300

    commit

--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -1,6 +1,11 @@
 package ru.job4j.serialization.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Person {
     private final boolean sex;
@@ -8,7 +13,7 @@ public class Person {
     private final Contact contact;
     private final String[] statuses;
 
-    public Person(boolean sex, int age, Contact contact, String[] statuses) {
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.contact = contact;
@@ -23,5 +28,39 @@ public class Person {
                 + ", contact=" + contact
                 + ", statuses=" + Arrays.toString(statuses)
                 + '}';
+    }
+
+    public static void main(String[] args) {
+
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject);
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(person));
+    }
+
+    private boolean isSex() {
+        return sex;
+    }
+
+    private int getAge() {
+        return age;
     }
 }
--- src/main/java/ru/job4j/serialization/xml/XmlEx.java
+++ src/main/java/ru/job4j/serialization/xml/XmlEx.java
@@ -2,6 +2,6 @@ package ru.job4j.serialization.xml;
 
 public class XmlEx {
     public static void main(String[] args) {
-        Car porche = new Car(false, 1, "Green", new Engine("V8", 3.2, "petrol"), new String[]{"40 gallons of petrol", "winter tire set"});
+        Car porche = new Car(false, 1, "Green", new Engine("V8", 3.2, "petrol"), "40 gallons of petrol", "winter tire set");
     }
 }
