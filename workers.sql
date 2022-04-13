6eaab8d5efc324c3dd02460c751d92abfcd5bb6a
aleksashk <40260846+aleksashk@users.noreply.github.com>
2022-04-13 15:57:06 +0300

    Update workers.sql
    changed file

--- workers.sql
+++ workers.sql
@@ -9,11 +9,20 @@ create table workers(
 
 select * from workers;
 
-insert into workers(first_name, last_name, married, department, serial_id) values('Frank','Sinatra', false, 'delivery deratrment', '01240112');
+insert into workers(first_name, last_name, married, department, serial_id) 
+	values('Frank','Sinatra', false, 'delivery deratrment', '01240112');
+
+select * from workers;
+
+update workers 
+	set first_name = 'Lenoks' 
+	where id = 1;
+update workers 	
+	set last_name = 'Luis' 
+	where id = 1;
 
 select * from workers;
 
 delete from workers;
 
 select * from workers;
-
