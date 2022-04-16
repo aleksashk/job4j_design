aa7dd1a111685f431a6928a199138a255745e405
aleksashk <40260846+aleksashk@users.noreply.github.com>
2022-04-16 11:28:18 +0300

    commit 16_04
    fixed queries

--- select_6861.sql
+++ select_6861.sql
@@ -27,10 +27,10 @@ select * from fauna
 	where name like '%fish%';
 
 select * from fauna 
-	where 10000 < avg_age 
-		and 21000 < avg_age;
+	where avg_age between 10000  
+		and 21000;
 
-select * from fauna where current_date is null;
+select * from fauna where discovery_date is null;
 
 select * from fauna where discovery_date < date'1950-01-01';
 
