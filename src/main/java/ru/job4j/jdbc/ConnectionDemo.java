96bf610d701686f52d233e0ba7384a2635427596
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-15 19:26:55 +0300

    0.2. PrepareStatement и остатки незакомиченных файлов

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,35 +1,21 @@
 package ru.job4j.jdbc;
 
+import ru.job4j.io.Config;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionDemo {
-    private Properties properties;
-
-    public Properties getProperties() {
-        return properties;
-    }
-
-    public void load() throws IOException {
-        properties = new Properties();
-        try (InputStream inputStream = ConnectionDemo.class.getClassLoader().getResourceAsStream("app.properties")) {
-            properties.load(inputStream);
-        }
-    }
-
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        ConnectionDemo connectionDemo = new ConnectionDemo();
-        connectionDemo.load();
-        String name = connectionDemo.getProperties().getProperty("jdbc.Driver");
-        String url = connectionDemo.getProperties().getProperty("jdbc.url");
-        String login = connectionDemo.getProperties().getProperty("jdbc.username");
-        String password = connectionDemo.getProperties().getProperty("jdbc.password");
-        Class.forName(name);
+        Config config = new Config("app.properties");
+        config.load();
+        Class.forName(config.value("driver"));
+        String url = config.value("url");
+        String login = config.value("login");
+        String password = config.value("password");
 
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
