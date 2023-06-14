b78a0a71e8fa0edf51bcdbe41fcf5c85ead2e0b6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-14 17:33:23 +0300

    Утверждения с примитивными типами second example

--- src/test/java/ru/job4j/assertj/ModelTest.java
+++ src/test/java/ru/job4j/assertj/ModelTest.java
@@ -10,5 +10,19 @@ class ModelTest {
         Model model = new Model(1, 5.255d, "name", true);
         boolean result = model.isCondition();
         assertThat(result).isTrue();
+
+        Model model1 = new Model(1, 6.234d, "I am learning Java", false);
+        String result1 = model1.getLine();
+        assertThat(result1).isNotNull()
+                .isNotEmpty()
+                .isNotBlank()
+                .containsIgnoringCase("java")
+                .contains("am", "Java")
+                .doesNotContain("javascript")
+                .startsWith("I am")
+                .startsWithIgnoringCase("i")
+                .endsWith("Java")
+                .isEqualTo("I am learning Java");
+
     }
 }
