2b5d4db46ad963ffde8649dddfb3b04580bcca2c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-14 17:34:14 +0300

    Утверждения с примитивными типами third example

--- src/test/java/ru/job4j/assertj/ModelTest.java
+++ src/test/java/ru/job4j/assertj/ModelTest.java
@@ -23,6 +23,17 @@ class ModelTest {
                 .startsWithIgnoringCase("i")
                 .endsWith("Java")
                 .isEqualTo("I am learning Java");
+    }
 
+    @Test
+    void checkInt() {
+        Model model = new Model(2, 5.2d, "name", true);
+        int result = model.getTop();
+        assertThat(result).isNotZero()
+                .isPositive()
+                .isEven()
+                .isGreaterThan(1)
+                .isLessThan(3)
+                .isEqualTo(2);
     }
 }
