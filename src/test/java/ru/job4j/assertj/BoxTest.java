8474e943001de2e632b8530c758aa7ab2cbdc2c8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-14 18:09:20 +0300

    3. Утверждения с примитивными типами [#504883  [#504883]] refactoring

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -11,57 +11,31 @@ class BoxTest {
     void isThisSphere() {
         Box box = new Box(0, 10);
         String name = box.whatsThis();
-        assertThat(name).isEqualTo("Sphere");
-    }
-
-    @Test
-    void isThisUnknown() {
-        Box box = new Box(3, 50);
-        String name = box.whatsThis();
-        int number = box.getNumberOfVertices();
-        assertThat(name).isEqualTo("Unknown object");
-        assertThat(number).isEqualTo(box.getNumberOfVertices());
+        assertThat(name).isEqualTo("Sphere")
+                .isNotEqualTo("Tetrahedron");
     }
 
     @Test
     void getNumberOfVerticesIs4() {
         Box box = new Box(4, 50);
         int vertices = box.getNumberOfVertices();
-        assertThat(vertices).isEqualTo(4);
-    }
-
-    @Test
-    void getNumberOfVerticesIs8() {
-        Box box = new Box(8, 50);
-        int vertices = box.getNumberOfVertices();
-        assertThat(vertices).isEqualTo(8);
+        assertThat(vertices).isEqualTo(4)
+                .isNotZero();
     }
 
     @Test
     void isExist() {
         Box box = new Box(4, 10);
         boolean isExist = box.isExist();
-        assertThat(isExist).isEqualTo(true);
-    }
-
-    @Test
-    void isNotExist() {
-        Box box = new Box(2, 100);
-        boolean isExist = box.isExist();
-        assertThat(isExist).isEqualTo(false);
+        assertThat(isExist).isEqualTo(true)
+                .isNotEqualTo(false);
     }
 
     @Test
     void getAreaWith4Vertex() {
         Box box = new Box(4, 10);
         double area = box.getArea();
-        assertThat(area).isEqualTo(173.205, withPrecision(0.001d));
-    }
-
-    @Test
-    void getAreaWith8Vertex() {
-        Box box = new Box(8, 340);
-        double area = box.getArea();
-        assertThat(area).isEqualTo(693600, withPrecision(0.00001d));
+        assertThat(area).isEqualTo(173.205, withPrecision(0.001d))
+                .isNotEqualTo(200);
     }
 }
